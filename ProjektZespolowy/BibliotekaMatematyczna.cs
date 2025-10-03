38f4cab284117eb6f91cfd399345827177a71b3c
Arturito-jiff <artur29.ak@gmail.com>
2025-10-03 15:13:22 +0200

    dodanie metody min i max

--- ProjektZespolowy/BibliotekaMatematyczna.cs
+++ ProjektZespolowy/BibliotekaMatematyczna.cs
@@ -41,5 +41,31 @@ namespace ProjektZespolowy
 
             return tab;
         }
+
+        public int Min(int[] tab)
+        {
+            int min = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (min > tab[i])
+                {
+                    min = tab[i];
+                }
+            }
+
+            return min;
+        }
+        public int Max(int[] tab)
+        {
+            int max = tab[0];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (max < tab[i])
+                {
+                    max = tab[i];
+                }
+            }
+            return max;
+        }
     }
 }
